87a3ef44a2e881fd5b0967823071b874da245c00
nexerax-collab <f.boehme@protonmail.com>
2025-04-12 22:18:57 +0200

    Update streamlit_pyPLM.py

--- streamlit_pyPLM.py
+++ streamlit_pyPLM.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 import sqlite3
-from pyPLM import create_database, get_db_connection, Item, BOM, ChangeRequest, add_item_to_db, add_change_request_to_db, get_document_from_db
+from pyPLM_with_states import create_database, get_db_connection, Item, BOM, ChangeRequest, add_item_to_db, add_change_request_to_db, get_document_from_db, load_bom_links
 
 st.set_page_config(page_title="PyPLM", layout="wide")
 if "splash_shown" not in st.session_state:
